c72ebc40cac2876062c74c400928a37c01e82e2c
Lucas Layman <laymanl@uncw.edu>
2018-11-25 21:30:23 -0500

    Added permission request/checking for lab exercise

--- app/src/main/java/edu/uncw/whereami/MainActivity.java
+++ app/src/main/java/edu/uncw/whereami/MainActivity.java
@@ -1,7 +1,11 @@
 package edu.uncw.whereami;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,6 +15,8 @@ import android.widget.Toast;
 import io.objectbox.Box;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 65535;
 
     RecyclerView mRecyclerView;
 
@@ -29,7 +35,38 @@ public class MainActivity extends AppCompatActivity {
                 DividerItemDecoration.VERTICAL));
     }
 
+
+
     public void recordClick(View view) {
-        Toast.makeText(this, "This button does nothing useful!", Toast.LENGTH_SHORT).show();
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "I need permission to access location in order to record locations.", Toast.LENGTH_SHORT).show();
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+        }
+        else {
+            Toast.makeText(this, "I should be recording the location now!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "I can record the location now!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "This app won't work until you grant permission!", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
     }
 }
