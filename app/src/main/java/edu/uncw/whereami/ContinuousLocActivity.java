67c0714a9139415f17e2c42a9bbc848dfd4905ce
laymanl <laymanl@uncw.edu>
2020-04-07 10:18:59 -0400

    Refactored continous update and on demand into two activities.

---
+++ app/src/main/java/edu/uncw/whereami/ContinuousLocActivity.java
@@ -0,0 +1,242 @@
+package edu.uncw.whereami;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.Query;
+
+import java.util.Date;
+import java.util.Locale;
+
+public class ContinuousLocActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 65535;
+    private static final String LOCATIONRECORD_COLLECTION = "location-recordings";
+    private static final String TAG = "MainActivity";
+    private static String LATITUDE_KEY = "latitude";
+    private static String LONGITUDE_KEY = "longitude";
+    private static String ACCURACY_KEY = "accuracy";
+
+    TextView latText;
+    TextView lonText;
+    TextView accuracyText;
+
+    private LocationCallback mLocationCallback;
+
+    private static int REQUEST_CHECK_SETTINGS = 123;
+    private LocationRequest mLocationRequest = new LocationRequest()
+            .setInterval(10000)
+            .setFastestInterval(5000)
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_continuous);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        latText = findViewById(R.id.latitude);
+        lonText = findViewById(R.id.longitude);
+        accuracyText = findViewById(R.id.acc);
+
+        // Setup the RecyclerView to display new and changed LocationRecords in the Firestore
+        final RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Query query = db.collection(LOCATIONRECORD_COLLECTION)
+                .orderBy("timestamp", Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions<LocationRecord> options = new FirestoreRecyclerOptions.Builder<LocationRecord>()
+                .setQuery(query, LocationRecord.class)
+                .setLifecycleOwner(this)
+                .build();
+        // The OnDataChangedListener makes the RecyclerCiew scroll to the topmost (newest) item
+        // automatically when one is added.
+        LocationRecordAdapter adapter = new LocationRecordAdapter(options, new LocationRecordAdapter.OnDataChangedListener() {
+            @Override
+            public void onDataChanged() {
+                recyclerView.scrollToPosition(0);
+            }
+        });
+        recyclerView.setAdapter(adapter);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this,
+                DividerItemDecoration.VERTICAL));
+
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult != null) {
+                    for (Location location : locationResult.getLocations()) {
+                        if (location != null) {
+                            latText.setText(String.format(Locale.US, "%.7f", location.getLatitude()));
+                            lonText.setText(String.format(Locale.US, "%.7f", location.getLongitude()));
+                            accuracyText.setText(String.format(Locale.US, "%.2f", location.getAccuracy()));
+
+                            LocationRecord lr = new LocationRecord(
+                                    new Date(location.getTime()),
+                                    new GeoPoint(location.getLatitude(), location.getLongitude()),
+                                    location.getAccuracy());
+
+                            db.collection(LOCATIONRECORD_COLLECTION)
+                                    .add(lr)
+                                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                        @Override
+                                        public void onSuccess(DocumentReference documentReference) {
+                                            Log.d(TAG, "Location Record added with ID: " + documentReference.getId());
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w(TAG, "Error adding LocationRecord", e);
+                                        }
+                                    });
+                        }
+                    }
+                }
+            }
+        };
+
+        if (savedInstanceState != null) {
+            latText.setText(savedInstanceState.getCharSequence(LATITUDE_KEY));
+            lonText.setText(savedInstanceState.getCharSequence(LONGITUDE_KEY));
+            accuracyText.setText(savedInstanceState.getCharSequence(ACCURACY_KEY));
+        }
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putCharSequence(LATITUDE_KEY, latText.getText());
+        outState.putCharSequence(LONGITUDE_KEY, lonText.getText());
+        outState.putCharSequence(ACCURACY_KEY, accuracyText.getText());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        createLocationRequest();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0  && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "I can record the location now!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "This app won't work until you grant permission!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+
+
+    /**
+     * This method tries to start the continuous location updates, which is a complex process.
+     * 1) Check to see if we can get location using the SettingsClient. Cases where you may not be able to get location
+     * include: Airplane mode is on, Location Services are turned off, or the device does not have GPS.
+     * 2) Next, check to see if the user has granted permission to access the location. If not, request permission. If so, start the location updates.
+     */
+    protected void createLocationRequest() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // All location settings are satisfied. The client can initialize
+                // location requests here.
+                // ...
+                if (ContextCompat.checkSelfPermission(ContinuousLocActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(ContinuousLocActivity.this, "I need permission to access location in order to record locations.", Toast.LENGTH_SHORT).show();
+
+                    ActivityCompat.requestPermissions(ContinuousLocActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+                } else {
+                    mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                            mLocationCallback,
+                            null /* Looper */);
+                }
+            }
+        }).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(ContinuousLocActivity.this,
+                                REQUEST_CHECK_SETTINGS);
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignore the error.
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CHECK_SETTINGS) {
+            // Regardless of whether the user fixed the issue or not, try again.
+            createLocationRequest();
+
+            // This could be really annoying for the user...
+            // In reality, you should check the resultCode for success. If not successful, you
+            // should degrade the functionality.
+        }
+    }
+}
